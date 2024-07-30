474556fa1dadcb40027a9a1aa76c6b2d3dec5312
Karl George <karlgeorge87@gmail.com>
2024-07-30 11:31:35 +0100

    Add bottom sheet component

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import AnimatedIntro from '../components/AnimatedIntro';
+import BottomLoginSheet from '../components/BottomLoginSheet';
 
 const Index = () => {
   return (
     <View style={{ flex: 1 }}>
       <AnimatedIntro />
+      <BottomLoginSheet />
     </View>
   );
 };
