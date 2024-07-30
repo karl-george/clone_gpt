2168c02901be6b8ae3937381abaa6bd0954591a7
Karl George <karlgeorge87@gmail.com>
2024-07-30 15:19:58 +0200

    Refactor to fix login error

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,11 @@
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { useSignIn, useSignUp } from '@clerk/clerk-expo';
 import { useLocalSearchParams } from 'expo-router';
 import React, { useState } from 'react';
 import {
   ActivityIndicator,
+  Alert,
   Image,
   KeyboardAvoidingView,
   Platform,
@@ -11,67 +15,112 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import Colors from '../constants/Colors';
-import { defaultStyles } from '../constants/Styles';
 
-const Page = () => {
+const Login = () => {
   const { type } = useLocalSearchParams<{ type: string }>();
+  const { signIn, setActive, isLoaded } = useSignIn();
+  const {
+    signUp,
+    isLoaded: signUpLoaded,
+    setActive: signupSetActive,
+  } = useSignUp();
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
-  const [emailAddress, setEmailAddress] = useState();
-  const [password, setPassword] = useState();
 
-  const onSignUpPress = async () => {};
+  const onSignInPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
+    setLoading(true);
+    try {
+      const completeSignIn = await signIn.create({
+        identifier: emailAddress,
+        password,
+      });
 
-  const onLoginPress = async () => {};
+      // This indicates the user is signed in
+      await setActive({ session: completeSignIn.createdSessionId });
+    } catch (err: any) {
+      Alert.alert(err.errors[0].message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onSignUpPress = async () => {
+    if (!signUpLoaded) {
+      return;
+    }
+    setLoading(true);
+
+    try {
+      // Create the user on Clerk
+      const result = await signUp.create({
+        emailAddress,
+        password,
+      });
+
+      // This indicates the user is signed in
+      signupSetActive({ session: result.createdSessionId });
+    } catch (err: any) {
+      alert(err.errors[0].message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       keyboardVerticalOffset={70}
-      style={[styles.container]}
+      style={styles.container}
     >
       {loading && (
         <View style={defaultStyles.loadingOverlay}>
           <ActivityIndicator size='large' color='#fff' />
         </View>
       )}
+
       <Image
         source={require('../assets/images/logo-dark.png')}
         style={styles.logo}
       />
+
       <Text style={styles.title}>
-        {type === 'login' ? 'Welcome Back' : 'Create your account'}
+        {type === 'login' ? 'Welcome back' : 'Create your account'}
       </Text>
       <View style={{ marginBottom: 30 }}>
         <TextInput
-          style={styles.inputField}
           autoCapitalize='none'
           placeholder='Email'
           value={emailAddress}
-          onChangeText={() => setEmailAddress}
+          onChangeText={setEmailAddress}
+          style={styles.inputField}
         />
         <TextInput
-          style={styles.inputField}
-          autoCapitalize='none'
           placeholder='Password'
           value={password}
-          onChangeText={() => setPassword}
+          onChangeText={setPassword}
           secureTextEntry
+          style={styles.inputField}
         />
       </View>
+
       {type === 'login' ? (
         <TouchableOpacity
           style={[defaultStyles.btn, styles.btnPrimary]}
-          onPress={onLoginPress}
+          onPress={onSignInPress}
         >
-          <Text>Login</Text>
+          <Text style={styles.btnPrimaryText}>Login</Text>
         </TouchableOpacity>
       ) : (
         <TouchableOpacity
           style={[defaultStyles.btn, styles.btnPrimary]}
           onPress={onSignUpPress}
         >
-          <Text>Create Account</Text>
+          <Text style={styles.btnPrimaryText}>Create account</Text>
         </TouchableOpacity>
       )}
     </KeyboardAvoidingView>
@@ -114,4 +163,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Page;
+export default Login;
