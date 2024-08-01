a2da06ad2750f8fb2be831b18d4f5c114ed909af
Karl George <karlgeorge87@gmail.com>
2024-08-01 13:39:04 +0200

    Add drop down menu component

--- app/(auth)/(drawer)/(chat)/new.tsx
+++ app/(auth)/(drawer)/(chat)/new.tsx
@@ -2,15 +2,33 @@ import HeaderDropDown from '@/components/HeaderDropDown';
 import { defaultStyles } from '@/constants/Styles';
 import { useAuth } from '@clerk/clerk-expo';
 import { Stack } from 'expo-router';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, View } from 'react-native';
 
 const Page = () => {
+  const [gptVersion, setGptVersion] = useState('3.5');
   const { signOut } = useAuth();
 
   return (
     <View style={defaultStyles.pageContainer}>
-      <Stack.Screen options={{ headerTitle: () => <HeaderDropDown /> }} />
+      <Stack.Screen
+        options={{
+          headerTitleAlign: 'center',
+          headerTitle: () => (
+            <HeaderDropDown
+              title='CloneGPT'
+              items={[
+                { key: '3.5', title: 'GPT-3.5', icon: 'bolt' },
+                { key: '4', title: 'GPT-4', icon: 'new-releases' },
+              ]}
+              onSelect={(key) => {
+                setGptVersion(key);
+              }}
+              selected={gptVersion}
+            />
+          ),
+        }}
+      />
       <Button
         title='Sign out'
         onPress={() => {
--- app/(auth)/(drawer)/dalle.tsx
+++ app/(auth)/(drawer)/dalle.tsx
@@ -1,10 +1,28 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { defaultStyles } from '@/constants/Styles';
+import HeaderDropDown from '@/components/HeaderDropDown';
+import { Stack } from 'expo-router';
 
 const Page = () => {
   return (
-    <View>
-      <Text>Dalle</Text>
+    <View style={defaultStyles.pageContainer}>
+      <Stack.Screen
+        options={{
+          headerTitleAlign: 'center',
+          headerTitle: () => (
+            <HeaderDropDown
+              title='DallE'
+              items={[
+                { key: 'share', title: 'Share GPT', icon: 'bolt' },
+                { key: 'details', title: 'See Details', icon: 'new-releases' },
+                { key: 'keep', title: 'Keep in Sidebar', icon: 'pin' },
+              ]}
+              onSelect={() => {}}
+            />
+          ),
+        }}
+      />
     </View>
   );
 };
