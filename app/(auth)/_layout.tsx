d2bcea1b90562b595a6be46932d09df9dbd5607f
Karl George <karlgeorge87@gmail.com>
2024-08-01 10:40:57 +0200

    Add drawer screen options

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,7 +2,11 @@ import { Stack } from 'expo-router';
 import React from 'react';
 
 const Layout = () => {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name='(drawer)' options={{ headerShown: false }} />
+    </Stack>
+  );
 };
 
 export default Layout;
