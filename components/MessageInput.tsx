07842500a29c5c0874f81ddace1f8056854c7673
Karl George <karlgeorge87@gmail.com>
2024-08-05 13:46:28 +0200

    Add picker animation

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -3,8 +3,16 @@ import { FontAwesome5, Ionicons } from '@expo/vector-icons';
 import { BlurView } from 'expo-blur';
 import React, { useState } from 'react';
 import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
-import Animated, { useSharedValue } from 'react-native-reanimated';
+import Animated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import * as ImagePicker from 'expo-image-picker';
+import * as DocumentPicker from 'expo-document-picker';
 
 export type MessageInputProps = {
   onShouldSend: (message: string) => void;
@@ -17,14 +25,58 @@ const MessageInput = ({ onShouldSend }: MessageInputProps) => {
   const { bottom } = useSafeAreaInsets();
   const expanded = useSharedValue(0);
 
-  const expandItems = () => {};
+  const expandItems = () => {
+    expanded.value = withTiming(1, { duration: 400 });
+  };
 
-  const collapseItems = () => {};
+  const collapseItems = () => {
+    expanded.value = withTiming(0, { duration: 400 });
+  };
+
+  const onChangeText = (text: string) => {
+    collapseItems();
+    setMessage(text);
+  };
 
   const onSend = () => {
     onShouldSend(message);
     setMessage('');
   };
+
+  const expandedButtonStyle = useAnimatedStyle(() => {
+    const opacityInterpolation = interpolate(
+      expanded.value,
+      [0, 1],
+      [1, 0],
+      Extrapolation.CLAMP
+    );
+
+    const widthInterpolation = interpolate(
+      expanded.value,
+      [0, 1],
+      [30, 0],
+      Extrapolation.CLAMP
+    );
+
+    return {
+      opacity: opacityInterpolation,
+      width: widthInterpolation,
+    };
+  });
+
+  const ButtonViewStyle = useAnimatedStyle(() => {
+    const widthInterpolation = interpolate(
+      expanded.value,
+      [0, 1],
+      [0, 100],
+      Extrapolation.CLAMP
+    );
+
+    return {
+      opacity: expanded.value,
+      width: widthInterpolation,
+    };
+  });
 
   return (
     <BlurView
@@ -33,16 +85,36 @@ const MessageInput = ({ onShouldSend }: MessageInputProps) => {
       style={{ paddingBottom: bottom + 6, paddingTop: 10 }}
     >
       <View style={styles.row}>
-        <ATouchableOpacity onPress={expandItems} style={[styles.roundBtn]}>
+        <ATouchableOpacity
+          onPress={expandItems}
+          style={[styles.roundBtn, expandedButtonStyle]}
+        >
           <Ionicons name='add' size={24} color={Colors.grey} />
         </ATouchableOpacity>
+
+        <Animated.View style={[styles.buttonView, ButtonViewStyle]}>
+          <TouchableOpacity onPress={() => ImagePicker.launchCameraAsync()}>
+            <Ionicons name='camera-outline' size={24} color={Colors.grey} />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => ImagePicker.launchImageLibraryAsync()}
+          >
+            <Ionicons name='image-outline' size={24} color={Colors.grey} />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => DocumentPicker.getDocumentAsync()}>
+            <Ionicons name='folder-outline' size={24} color={Colors.grey} />
+          </TouchableOpacity>
+        </Animated.View>
+
         <TextInput
           autoFocus
           placeholder='Message'
           style={styles.messageInput}
           multiline
           value={message}
-          onChangeText={setMessage}
+          onChangeText={onChangeText}
           onFocus={collapseItems}
         />
         {message.length > 0 ? (
@@ -85,6 +157,11 @@ const styles = StyleSheet.create({
     borderWidth: StyleSheet.hairlineWidth,
     borderColor: Colors.greyLight,
     backgroundColor: Colors.light,
+  },
+  buttonView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
   },
 });
 
