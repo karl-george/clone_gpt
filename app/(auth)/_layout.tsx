34192220656f08e026a51052a2e134e1341a63a9
Karl George <karlgeorge87@gmail.com>
2024-08-06 12:59:46 +0200

    Add settings header styling

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,39 @@
-import { Stack } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { Stack, useRouter } from 'expo-router';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 
 const Layout = () => {
+  const router = useRouter();
+
   return (
     <Stack>
       <Stack.Screen name='(drawer)' options={{ headerShown: false }} />
+      <Stack.Screen
+        name='(modal)/settings'
+        options={{
+          headerTitle: 'Settings',
+          presentation: 'modal',
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: Colors.selected,
+          },
+          headerTitleAlign: 'center',
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => router.back()}
+              style={{
+                backgroundColor: Colors.greyLight,
+                borderRadius: 20,
+                padding: 4,
+              }}
+            >
+              <Ionicons name='close-outline' size={20} color={Colors.grey} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </Stack>
   );
 };
