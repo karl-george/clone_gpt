beb53e17667f4a1ab665364fb11be70488d614bf
Karl George <karlgeorge87@gmail.com>
2024-08-08 13:26:12 +0200

    Add resizing image

--- app/(auth)/(modal)/[url].tsx
+++ app/(auth)/(modal)/[url].tsx
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ImageZoom } from '@likashefqet/react-native-image-zoom';
 
 const Page = () => {
   const { url, prompt } = useLocalSearchParams<{
@@ -16,10 +17,33 @@ const Page = () => {
   };
 
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      <ImageZoom
+        uri={url}
+        style={styles.image}
+        minScale={0.5}
+        maxScale={5}
+        minPanPointers={1}
+        doubleTapScale={2}
+        isSingleTapEnabled
+        isDoubleTapEnabled
+        resizeMode='contain'
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Page;
