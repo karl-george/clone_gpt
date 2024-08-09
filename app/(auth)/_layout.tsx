e497fc8505a542480b19ddcc29e8ed48fda404c0
Karl George <karlgeorge87@gmail.com>
2024-08-09 13:09:52 +0200

    Add db

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,42 +1,44 @@
 import Colors from '@/constants/Colors';
-import { Ionicons } from '@expo/vector-icons';
 import { Stack, useRouter } from 'expo-router';
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { SQLiteProvider } from 'expo-sqlite';
+import { migrateDbIfNeeded } from '@/utils/Database';
 
 const Layout = () => {
   const router = useRouter();
 
   return (
-    <Stack>
-      <Stack.Screen name='(drawer)' options={{ headerShown: false }} />
-      <Stack.Screen
-        name='(modal)/settings'
-        options={{
-          headerTitle: 'Settings',
-          presentation: 'modal',
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: Colors.selected,
-          },
-          headerTitleAlign: 'center',
-        }}
-      />
-      <Stack.Screen
-        name='(modal)/[url]'
-        options={{
-          headerTitle: '',
-          presentation: 'fullScreenModal',
-          headerTransparent: true,
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: 'rgba(0, 0, 0, 0.4)',
-          },
-          headerBlurEffect: 'dark',
-          headerTitleAlign: 'center',
-        }}
-      />
-    </Stack>
+    <SQLiteProvider databaseName='chats.db' onInit={migrateDbIfNeeded}>
+      <Stack>
+        <Stack.Screen name='(drawer)' options={{ headerShown: false }} />
+        <Stack.Screen
+          name='(modal)/settings'
+          options={{
+            headerTitle: 'Settings',
+            presentation: 'modal',
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: Colors.selected,
+            },
+            headerTitleAlign: 'center',
+          }}
+        />
+        <Stack.Screen
+          name='(modal)/[url]'
+          options={{
+            headerTitle: '',
+            presentation: 'fullScreenModal',
+            headerTransparent: true,
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: 'rgba(0, 0, 0, 0.4)',
+            },
+            headerBlurEffect: 'dark',
+            headerTitleAlign: 'center',
+          }}
+        />
+      </Stack>
+    </SQLiteProvider>
   );
 };
 
